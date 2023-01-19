ee85c5bc664e1b5dcc8f79f83fd5106b91a3cf1a
malkolmz <kurban.63.axmedov@gmail.com>
2023-01-19 12:10:31 +0300

    done

--- articles/admin.py
+++ articles/admin.py
@@ -2,24 +2,34 @@ from django.contrib import admin
 from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 from .models import Article, Scope
+from pprint import pprint
 
 
-# class RelationshipInlineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         for form in self.forms:
-#             # В form.cleaned_data будет словарь с данными
-#             # каждой отдельной формы, которые вы можете проверить
-#             form.cleaned_data
-#             # вызовом исключения ValidationError можно указать админке о наличие ошибки
-#             # таким образом объект не будет сохранен,
-#             # а пользователю выведется соответствующее сообщение об ошибке
-#             raise ValidationError('Тут всегда ошибка')
-#         return super().clean()  # вызываем базовый код переопределяемого метода
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_entry = 0
+
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            pprint(form.cleaned_data)
+
+            # form.cleaned_data
+            if form.cleaned_data.get('is_main'):
+                main_entry += 1
+
+        if main_entry != 1:
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            raise ValidationError('Тут всегда ошибка')
+        return super().clean()  # вызываем базовый код переопределяемого метода
 
 
 class ScopeInline(admin.TabularInline):
     model = Scope
-    # formset = RelationshipInlineFormset
+    extra = 1
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
